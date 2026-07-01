1b79193104ded09cecfa3866f941d5356eb669a1
millywanderi <wanderinyambura5@gmail.com>
2026-07-01 13:57:15 +0300

    Created the role class

---
+++ backend/app/models/role.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+
+    name: Mapped[str] = mapped_column(
+        String(50),
+        unique=True,
+        nullable=False
+    )
