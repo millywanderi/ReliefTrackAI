bd795f0579e9383559a9932286cb70dfc69ae8f4
millywanderi <wanderinyambura5@gmail.com>
2026-07-06 15:43:30 +0300

    Updated the endpoint to be router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
-from app.api.v1.endpoints import auth
+from app.api.v1.router import api_router
 
 app = FastAPI(
     title="ReliefTrack AI API",
     version="1.0.0",
     description="AI-powered Humanitarian Resource Management Platform",
 )
-app.include_router(auth.router)
+
+app.include_router(api_router, prefix="/api/v1")
 
 
 @app.get("/")
