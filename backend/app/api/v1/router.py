bbf5970874eb43c5ecf3bd5347941350bab9da6b
millywanderi <wanderinyambura5@gmail.com>
2026-07-07 14:21:28 +0300

    Registered the household members to the router

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -5,9 +5,11 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import auth
 from app.api.v1.endpoints import beneficiaries
 from app.api.v1.endpoints import households
+from app.api.v1.endpoints import household_members
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(beneficiaries.router)
 api_router.include_router(households.router)
+api_router.include_router(household_members.router)
