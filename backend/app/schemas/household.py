286cd17df310a064ea26bea26f3740c9abef5476
millywanderi <wanderinyambura5@gmail.com>
2026-07-07 14:34:37 +0300

    Update the Household Schema

--- backend/app/schemas/household.py
+++ backend/app/schemas/household.py
@@ -3,30 +3,34 @@
 from pydantic import BaseModel
 
 
+# -------------------------
+# CREATE HOUSEHOLD
+# -------------------------
 class HouseholdCreate(BaseModel):
-
     household_code: str
     household_head_id: int
-    household_size: int
-
     shelter_type: str | None = None
     livelihood: str | None = None
 
 
+# -------------------------
+# UPDATE HOUSEHOLD
+# -------------------------
 class HouseholdUpdate(BaseModel):
-
-    household_size: int | None = None
+    household_code: str | None = None
+    household_head_id: int | None = None
     shelter_type: str | None = None
     livelihood: str | None = None
 
 
+# -------------------------
+# RESPONSE
+# -------------------------
 class HouseholdResponse(BaseModel):
-
     id: int
     household_code: str
     household_head_id: int
     household_size: int
-
     shelter_type: str | None = None
     livelihood: str | None = None
 
