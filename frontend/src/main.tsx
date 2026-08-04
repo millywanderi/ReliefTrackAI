c9db58fbbe89db797b635937a729b3811fe7e9b7
millywanderi <wanderinyambura5@gmail.com>
2026-08-04 13:33:27 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { router } from "./app/router";
+import { Providers } from "./app/providers";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
+  </React.StrictMode>,
+);
