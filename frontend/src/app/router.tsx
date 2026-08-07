6dc68ce8ba549e17cbd4d3d161a9357a25f89e70
millywanderi <wanderinyambura5@gmail.com>
2026-08-07 11:26:32 +0300

    Update the router

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,25 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import AppLayout from "@/layouts/AppLayout";
-import DashboardPage from "@/features/dashboard/pages/DashboardPage";
+
 import LoginPage from "@/features/auth/pages/LoginPage";
+import ProtectedRoute from "@/features/auth/ProtectedRoute";
+
+import DashboardPage from "@/features/dashboard/pages/DashboardPage";
 
 export const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage />,
   },
+
   {
-    path: "/",
-    element: <AppLayout />,
+    element: <ProtectedRoute />,
     children: [
       {
-        index: true,
-        element: <DashboardPage />,
-      },
-      {
-        path: "dashboard",
-        element: <DashboardPage />,
+        path: "/",
+        element: <AppLayout />,
+        children: [
+          {
+            index: true,
+            element: <DashboardPage />,
+          },
+          {
+            path: "dashboard",
+            element: <DashboardPage />,
+          },
+        ],
       },
     ],
   },
