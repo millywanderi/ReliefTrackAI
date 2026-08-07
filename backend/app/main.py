3a9b56213745553ef8ad47a7d91ee1002a371f77
millywanderi <wanderinyambura5@gmail.com>
2026-08-07 11:23:07 +0300

    Updated the app to connect frontend authentication

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,22 @@
 
 from fastapi import FastAPI
 from app.api.v1.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="ReliefTrack AI API",
     version="1.0.0",
     description="AI-powered Humanitarian Resource Management Platform",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api/v1")
