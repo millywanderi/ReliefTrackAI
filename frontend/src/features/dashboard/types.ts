439be6ac42cfe9770388ed0040476a803681fac5
millywanderi <wanderinyambura5@gmail.com>
2026-08-08 12:55:27 +0300

    Create the dashboard type

---
+++ frontend/src/features/dashboard/types.ts
@@ -0,0 +1,10 @@
+export interface DashboardData {
+  beneficiaries: number;
+  households: number;
+  warehouses: number;
+  resources: number;
+  distribution_events: number;
+  resources_distributed: number;
+  low_stock_alerts: number;
+  critical_beneficiaries: number;
+}
